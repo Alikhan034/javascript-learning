2e8a69f46703b497d929147d2745ce8dea40378a
Alikhan034 <malikhan03400@gmail.com>
2025-03-21 16:03:55 +0500

    for of loop vs for in loop

--- 13.oop.js
+++ 13.oop.js
@@ -61,3 +61,13 @@ console.log(Object.entries(per2));*/
 for (const key of Object.keys(per2)) {
   console.log("key: ", per2[key]);
 }
+
+const obj = { a: 1, b: 2 };
+for (let key in obj) {
+  console.log(key); // "a", "b"
+}
+
+const arr = [10, 20, 30];
+for (let val of arr) {
+  console.log(val); // 10, 20, 30
+}
