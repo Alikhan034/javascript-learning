7177a35d069bf4d651ae8c999b052aca97ce381e
Alikhan034 <malikhan03400@gmail.com>
2025-03-23 22:23:40 +0500

    spread and rest operator

--- 15.spread&restoperator.js
+++ 15.spread&restoperator.js
@@ -8,7 +8,13 @@ function showitem(place1, place2, place3, place4, place5, place6, place7) {
   console.log(place6);
   console.log(place7);
 }
+//+ two arrays into single array
 let top7 = [...top3, "England", "UAE", "Newzeland", "Italy"];
 
 //showitem(top3[0], top3[1], top3[2]);
 showitem(...top7);
+
+const { a, b, ...rest } = { a: 1, b: 2, c: 3, d: 4 };
+console.log(a); // Output: 1
+console.log(b); // Output: 2
+console.log(rest); // Output: { c: 3, d: 4 }
