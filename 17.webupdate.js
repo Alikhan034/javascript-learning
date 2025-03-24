423d561122a00307a988c16ac7a0a3579fccca78
Alikhan034 <malikhan03400@gmail.com>
2025-03-25 01:00:19 +0500

    capture data

--- 17.webupdate.js
+++ 17.webupdate.js
@@ -11,3 +11,26 @@ document.body.appendChild(input);
 input.addEventListener("change", function () {
   h1.innerText = input.value;
 });
+
+var h1 = document.querySelector("h1");
+
+var arr = ["Example Domain", "First Click", "Second Click", "Third Click"];
+
+function handleClicks() {
+  switch (h1.innerText) {
+    case arr[0]:
+      h1.innerText = arr[1];
+      break;
+    case arr[1]:
+      h1.innerText = arr[2];
+      break;
+    case arr[2]:
+      h1.innerText = arr[3];
+      break;
+    default:
+      h1.innerText = arr[0];
+      break;
+  }
+}
+
+h1.addEventListener("click", handleClicks);
